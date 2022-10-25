053faf8df1c3fed219e89a0ae868e2376af455ab
mao <1296193245@qq.com>
2022-10-25 13:38:30 +0800

    spring_boot_starter_demo3

---
+++ ip-spring-boot-starter/src/main/java/mao/ipspringbootstarter/IpSpringBootStarterApplication.java
@@ -0,0 +1,15 @@
+package mao.ipspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpSpringBootStarterApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(IpSpringBootStarterApplication.class, args);
+    }
+
+}
